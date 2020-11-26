49b2f1eeed6e427466d156e8790ee5d2ad9ad950
FahimFBA <fahimbinamin@gmail.com>
2020-11-26 20:18:24 +0600

    added another solve in java

---
+++ 1004.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class 1004 {
+
+    public static void main(String[] args) throws IOException {
+
+        int a, b, x;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        x = a * b;
+        System.out.print("PROD = "+x+"\n");
+
+    }
+
+}
