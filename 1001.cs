dc1461a030e8ec4fdb9688c382caa11e7d59de68
FahimFBA <fahimbinamin@gmail.com>
2021-01-04 20:23:37 +0600

    1001 added of C#

---
+++ 1001.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args){
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result = a + b;
+            Console.WriteLine("X = " + result);
+        }
+
+}
