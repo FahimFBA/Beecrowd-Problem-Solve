58f56ee488bf13c766cad5bb4d6d175582095cd9
FahimFBA <fahimbinamin@gmail.com>
2021-01-06 03:09:06 +0600

    1002 - Documentation

--- cSharp1002.cs
+++ cSharp1002.cs
@@ -4,9 +4,9 @@ class URI {
 
     static void Main(string[] args)  {
             double r, a;
-            r = Convert.ToDouble(Console.ReadLine());
+            r = Convert.ToDouble(Console.ReadLine()); // Taking input from the user and converting that input to Double 
             a = 3.14159 * r * r;
-            Console.WriteLine("A=" + a.ToString("0.0000"));
+            Console.WriteLine("A=" + a.ToString("0.0000")); // converting a to String and also directing 4 digits after the fraction dot
 
             Console.ReadLine();
         }
