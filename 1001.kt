5b7f082b715bc6dfc9157555be89af69cf155c32
FahimFBA <fahimbinamin@gmail.com>
2021-02-24 23:41:41 +0600

    solved 1001 in kt

---
+++ 1001.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+ var a: Int
+    var b: Int
+    var sum: Int
+
+    var sc = Scanner(System.`in`)
+
+    
+    a = sc.nextInt()
+    b = sc.nextInt()
+
+    sum = a + b
+
+    println("X = $sum")
+	
+}
