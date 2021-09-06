8d4871b058271dc135a700e6aeb481651e67d2fa
FahimFBA <fahimbinamin@gmail.com>
2021-09-06 15:14:27 +0600

    Verdict: Accepted

---
+++ 1238.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char Word1[50], Word2[50];
+    int test_case, a, b, i, j;
+    scanf("%d", &test_case);
+    for (i = 0; i < test_case; i++)
+    {
+        scanf("%s", &Word1);
+        scanf("%s", &Word2);
+        for (a = 0, b = 0, j = 0; j < 50; j++)
+        {
+            if (Word1[j] == '\0')
+            {
+                a = 1;
+            }
+            if (Word2[j] == '\0')
+            {
+                b = 1;
+            }
+            if (a == 1 && b == 1)
+            {
+                break;
+            }
+            if (a == 0)
+            {
+                cout << Word1[j];
+            }
+            if (b == 0)
+            {
+                cout << Word2[j];
+            }
+        }
+        cout << endl;
+    }
+}
