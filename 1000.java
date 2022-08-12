21c70fa83aaf6b27745ae9360ad100c6bf4b3a2d
FahimFBA <fahimbinamin@gmail.com>
2022-08-12 11:43:06 +0600

    1000 in java

---
+++ 1000.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
