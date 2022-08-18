8f86c5a060fd3a15391d204bd966a32aecc5e2b7
FahimFBA <fahimbinamin@gmail.com>
2022-08-18 13:35:48 +0600

    added 1001

---
+++ 1001.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int X = A + B;
+        System.out.println("X = " + X);
+    }
+ 
+}
